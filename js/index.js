31ac88591b50eeda6fad1312552bf457175ffec8
Peter Krautzberger <p.krautzberger@gmail.com>
2015-08-24 14:08:33 +0200

    Fix preview
    Towards fixing #9: delay the code for the live preview until after MathJax is available.

--- js/index.js
+++ js/index.js
@@ -1,17 +1,14 @@
-!function () {
-	var demoSource = document.querySelector('#demoSource'),
-	    demoRendering = document.querySelector('#demoRendering'),
-	    math
-
-	MathJax.Hub.Queue(function () {
-		math = MathJax.Hub.getAllJax("demoRendering")[0]
-
-
-		demoSource.addEventListener('input', function () {
-
-			console.log(demoSource.value)
-
-			MathJax.Hub.Queue(["Text", math, demoSource.value])
-		})
-	})
-}()
+window.MathJax = {
+  AuthorInit: function() {
+    MathJax.Hub.Register.StartupHook("End", function() {
+      MathJax.Hub.processSectionDelay = 0;
+      var demoSource = document.getElementById('demoSource');
+      var demoRendering = document.getElementById('demoRendering');
+      var math = MathJax.Hub.getAllJax("demoRendering")[0];
+      demoSource.addEventListener('input', function() {
+        //console.log(demoSource.value);
+        MathJax.Hub.Queue(["Text", math, demoSource.value]);
+      });
+    });
+  }
+};
